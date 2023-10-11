1a3df35adfb0928dcccb587f2a3b6d36924a4302
w0zzer1 <48273822+w0zzer1@users.noreply.github.com.>
2023-10-11 19:57:35 +0200

    RGB mode implemented, everything working except can't change back from rgb to normal mode

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let container = document.querySelector(".container")
 let resetBtn = document.querySelector(".reset")
-let divs = document.querySelectorAll(".divElement");
+let rgbBtn = document.querySelector(".rgb")
+let normalBtn = document.querySelector(".normal")
 
+var primaryMouseButtonDown = false;
 
-generateGrid();
-mouseOverAdd();
+function setPrimaryButtonState(e) {
+  var flags = e.buttons !== undefined ? e.buttons : e.which;
+  primaryMouseButtonDown = (flags & 1) === 1;
+}
+document.addEventListener("mousedown", setPrimaryButtonState);
+document.addEventListener("mousemove", setPrimaryButtonState);
+document.addEventListener("mouseup", setPrimaryButtonState);
 
 resetBtn.addEventListener('click', () => {
     size = prompt("Size? :");
@@ -13,11 +20,17 @@ resetBtn.addEventListener('click', () => {
     mouseOverAdd();
 });
 
+rgbBtn.addEventListener('click', ()=>{rgbDivChanger()});
+normalBtn.addEventListener('click', ()=>{mouseOverAdd()});
+
 function generateGrid(size = 16){
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    container.appendChild(box)
     for (let i = 0; i < size; i++){
         let divRow = document.createElement("div");
         divRow.setAttribute("class", "divRow");
-        container.appendChild(divRow);
+        box.appendChild(divRow);
         for (let j = 0; j < size; j++){
             let divElement = document.createElement("div");
             divElement.setAttribute("class", "divElement");
@@ -27,21 +40,36 @@ function generateGrid(size = 16){
 }
 
 
+
 function mouseOverAdd() {
+    let divs = document.querySelectorAll(".divElement");    
+    divs.forEach((div) =>{
+        div.addEventListener('mouseover', (event)=> {
+            if (primaryMouseButtonDown){
+                div.setAttribute("class", "divElementHover")
+            }}            
+        )
+    });    
+}
+
+function rgbDivChanger(){
     let divs = document.querySelectorAll(".divElement");
     divs.forEach((div) =>{
-        div.addEventListener('mouseover', (event)=>
-            div.setAttribute("class", "divElementHover")
+        div.addEventListener('mouseover', (event)=>{
+        if (primaryMouseButtonDown){
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            div.style.backgroundColor = "#" + randomColor;
+            }}
         )
-    });
+    })
 }
 
 
 function removeGrid(){
-    let divRows = document.querySelectorAll(".divRow")
-    divRows.forEach((divRow) => {
-        container.removeChild(divRow);
-    });
-}
+    let box = document.querySelector(".box");
+    container.removeChild(box);
+};
 
 
+generateGrid();
+mouseOverAdd();
